6337267a98a02ceb8c059116c02a596170e7cc7e
crunchytee <tosh.robertsbrockway@gmail.com>
2021-09-19 15:42:50 -0700

    add tailwind and react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="md:w-fit-content md:mx-auto">
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 md:w-fit-content"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        {/* register your input into the hook by invoking the "register" function */}
+        <input
+          className="w-100% md:w-fit-content block shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          defaultValue="test"
+          {...register("example")}
+        />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input
+          className="w-100% md:w-fit-content block shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          {...register("exampleRequired", { required: true })}
+        />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <button
+          className="block bg-blue-500 w-100% hover:bg-buttonHover text-white font-bold py-2 px-4 my-4 rounded"
+          type="submit"
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
